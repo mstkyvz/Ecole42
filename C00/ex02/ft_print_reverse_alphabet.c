9b00184162f59187e4f8112b34ada5492aef1de1
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2022-08-29 14:27:53 +0300

    Ecole 42 Piscine Pool

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyavuz <muyavuz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/05 10:43:47 by muyavuz           #+#    #+#             */
+/*   Updated: 2022/08/25 01:19:56 by muyavuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyavuz <muyavuz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/05 11:38:29 by muyavuz           #+#    #+#             */
+/*   Updated: 2022/08/25 01:19:53 by muyavuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	negative;
+	char	positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+	{
+		write(1, &negative, 1);
+	}
+	else
+	{
+		write(1, &positive, 1);
+	}
+}
