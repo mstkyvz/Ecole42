d6f6a0ad7dd3863a089be440e0f2df77128454eb
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2022-09-07 02:16:08 +0300

    updatee

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -23,23 +23,24 @@ void	ft_putnbr(int nb)
 	{
 		ft_putchar('-');
 		ft_putchar('2');
-		ft_putnbr(147483648);
+		nb = 147483648;
 	}
-	else if (nb < 0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		nb = -nb;
-		ft_putnbr(nb);
+		nb = nb * -1;
 	}
-	else if (nb > 9)
+	if (nb < 10)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putchar(nb + 48);
+		return ;
 	}
 	else
-		ft_putchar(nb + 48);
+		ft_putnbr(nb / 10);
+	ft_putnbr(nb % 10);
 }
-/*int main (void)
+/*
+int	main(void)
 {
-	ft_putnbr(4563);
+	ft_putnbr(-2147483648);
 } */
