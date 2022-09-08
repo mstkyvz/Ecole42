3cfee818b03e96e991d538bc4839f2a2bf5cdeab
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2022-09-08 17:32:54 +0300

    %70 completed

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyavuz <muyavuz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 11:37:06 by muyavuz           #+#    #+#             */
+/*   Updated: 2022/09/06 23:50:12 by muyavuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = nb * -1;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+		return ;
+	}
+	else
+		ft_putnbr(nb / 10);
+	ft_putnbr(nb % 10);
+}
+/*
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+} */
