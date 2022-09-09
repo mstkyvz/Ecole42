e0be8e6a2f31e505dd5bf3474822ae77691feeda
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2022-09-09 12:58:59 +0300

    Update and rename ft_rev_print.c to rev_print.c

--- Exam02/rev_print.c
+++ Exam02/rev_print.c
@@ -27,3 +27,23 @@ int main()
 	char str[] = "gbrireugneb";
 	ft_rev_print(str);
 }
+
+-------------------------------------OR-----------------------------------
+	
+#include <unistd.h>
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i;
+
+		i = 0;
+		while(argv[1][i])
+			i += 1;
+		while(i)
+			write(1, &argv[1][--i], 1);
+	}
+	write(1 ,"\n", 1);
+	return (0);
+}
