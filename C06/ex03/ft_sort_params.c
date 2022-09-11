e33d39309df3c3aeca7ed4aacf66a8e0ab17f04b
Walker835 <47429110+mstkyvz@users.noreply.github.com>
2022-09-11 05:45:18 +0300

    %100 completed

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muyavuz <muyavuz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 15:49:02 by muyavuz           #+#    #+#             */
+/*   Updated: 2022/09/11 03:55:53 by muyavuz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		ft_putchar(str[i++]);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	j;
+	int	x;
+
+	x = 1;
+	while (x < argc -1)
+	{
+		j = 1;
+		while (j < argc -1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				ft_swap(&argv[j], &argv[j + 1]);
+			}
+			j++;
+		}
+		x++;
+	}
+	j = 1;
+	while (j < argc)
+	{
+		ft_putstr(argv[j]);
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
